18a1ea574a606e013b3c3620a6d55b9495b9c804
xuejiaming <326308290@qq.com>
2022-07-07 11:18:36 +0800

    1

--- abp/toapp/src/TodoApp.Domain.Shared/IShardingKeyIsGuId.cs
+++ abp/toapp/src/TodoApp.Domain.Shared/IShardingKeyIsGuId.cs
@@ -10,7 +10,7 @@ namespace TodoApp
     /// Author: xjm
     /// Created: 2022/7/6 13:56:35
     /// Email: 326308290@qq.com
-    public class IShardingKeyIsGuId
+    public interface IShardingKeyIsGuId
     {
     }
 }
--- abp/toapp/src/TodoApp.Domain/TodoItem.cs
+++ abp/toapp/src/TodoApp.Domain/TodoItem.cs
@@ -3,7 +3,7 @@ using Volo.Abp.Domain.Entities;
 
 namespace TodoApp
 {
-    public class TodoItem : BasicAggregateRoot<Guid>
+    public class TodoItem : BasicAggregateRoot<Guid>,IShardingKeyIsGuId,IShardingKeyIsCreationTime
     {
         public string Text { get; set; }
     }
