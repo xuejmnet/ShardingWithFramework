51d8a516bc3cdd207b8a4fb1d22a9147861cefe1
xuejiaming <326308290@qq.com>
2022-07-23 14:08:45 +0800

    1

--- furion/EFCoreMigrateMultiDatabase/EFCoreMigrateMultiDatabase/User.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace EFCoreMigrateMultiDatabase;
-[Table(nameof(User))]
-public class User
-{
-    public string UserId { get; set; }
-    public string UserName { get; set; }
-}
--- furion/EFCoreMigrateMultiDatabase/README.md
+++
@@ -1,2 +0,0 @@
-# EFCoreMigrateMultiDatabase
-EFCoreMigrateMultiDatabase
